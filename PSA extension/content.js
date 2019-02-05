d3e2ecd1b2e78a3854277c92c796dc9930584a31
Julien_Peluso <julien.maule@gmail.com>
2019-02-05 14:59:19 +0100

    First functionnal version without listener
    button won't work it executes only by clicking on the icon of the extension and need to setup the extention by right clicking on it>"possibilité de lire et de modifier les données du site" and select "when clicking on the extension" and then, click on the extension only when you are on the timetable

---
+++ PSA extension/content.js
@@ -0,0 +1,1 @@
+console.log("we are on content.js")
--- PSA extension/work.js
+++ PSA extension/work.js
@@ -1,104 +1,102 @@
-javascript: (function() {
 
-	/* Configuration */
 
-	var time = 7;
-    //var localSunday = documents.getElementById('');
-	var locations = [ /* C : Client, O : CGI, T : Télétravail, NA : N/A */
-		'NA', /* Dimanche */
-		'O', /* Lundi */
-		'O', /* Mardi */
-		'O', /* Mercredi */
-		'O', /* Jeudi */
-		'O', /* Vendredi */
-		'NA' /* Samedi */
-	];
+/* Configuration */
+console.log('work');
+var time = 8;
+//var localSunday = documents.getElementById('');
+var locations = [ /* C : Client, O : CGI, T : Télétravail, NA : N/A */
+	'NA', /* Dimanche */
+	'O', /* Lundi */
+	'O', /* Mardi */
+	'O', /* Mercredi */
+	'O', /* Jeudi */
+	'O', /* Vendredi */
+	'NA' /* Samedi */
+];
 
-	/* Code */
+/* Code */
 
-	var documents = [document];
-	var iframes = document.getElementsByTagName('iframe');
-	for (var iframeIndex = 0; iframeIndex < iframes.length; iframeIndex++) {
-		documents.push(iframes[iframeIndex].contentDocument);
+var documents = [document];
+var iframes = document.getElementsByTagName('iframe');
+for (var iframeIndex = 0; iframeIndex < iframes.length; iframeIndex++) {
+	documents.push(iframes[iframeIndex].contentDocument);
+}
+var $ = function (selector) {
+	selector = selector.substr(1).replace(/\\\$/g, '$');
+	console.log(selector);
+	for (var documentIndex = 0; documentIndex < documents.length; documentIndex++) {
+		var elem = documents[documentIndex].getElementById(selector);
+		if (elem) {
+			return elem;
+		}
 	}
-	var $ = function(selector) {
-		selector = selector.substr(1).replace(/\\\$/g, '$');
-		console.log(selector);
-		for (var documentIndex = 0; documentIndex < documents.length; documentIndex++) {
-			var elem = documents[documentIndex].getElementById(selector);
-			if (elem) {
-				return elem;
-			}
-		}
-		console.log('not found');
-		return null;
-	};
-	console.log(documents);
+	console.log('not found');
+	return null;
+};
+console.log(documents);
 
-	/* TIMETABLE */
-	var timeTable = $('#l0EX_TIME_DTL\\$0');
+/* TIMETABLE */
+var timeTable = $('#l0EX_TIME_DTL\\$0');
 
-	var updateTime = function() {
-        console.log('onUpdateTime');
-		updateTimeOnLine(0);
-	};
-	var updateTimeOnLine = function(line) {
-        console.log('onUpdateTimeOnLine');
-		for (var column = 2; column <= 6; column++) {
-			setValue('TIME', column, line, time);
-		}
-	};
-
-	var restTable = $('#UC_EX_TDLY_FR\\$scroll\\$0');
-
-	var updateRestLunchLocation = function() {  //methode pour changer la location (cgi, site client...etc)
-		for (var column = 1; column <= 7; column++) {
-			var location = locations[column - 1];
-			updateRestOnColumn(column, location == 'NA' ? 'NA' : 'Y');
-			updateLunchOnColumn(column, location == 'NA' ? 0 : 1);
-			updateLocationOnColumn(column, location);
-		}
-
-		/* Overtime requested by manager */
-		$('#UC_EX_TIME_FR_H_UC_OT_RQST_MNGR\\$0').value = 0;
-		/* including Sunday hours */
-		$('#UC_EX_TIME_FR_H_UC_OT_RM_SUNDAY\\$0').value = 0;
-		/* including public holidays but Sunday */
-		$('#UC_EX_TIME_FR_H_UC_OT_RM_PUBLIC_HO\\$0').value = 0;
-	};
-
-	var updateLunchOnColumn = function(column, value) {
-		setValue('UC_TIME_LIN_WRK_UC_DAILYREST1', column, 0, value);
-	};
-
-	var updateRestOnColumn = function(column, value) {
-		for (var line = 0; line < 3; line++) {
-			setValue('UC_DAILYREST', column, line, value);
-		}
-	};
-
-	var updateLocationOnColumn = function(column, value) {
-		setValue('UC_LOCATION_A', column, 0, value);
-		setValue('UC_LOCATION_A', column, 1, value);
-	};
-
-	var setValue = function(name, column, line, value) {
-		var id = '#'+ name + column + '$' + line;
-		console.log(id);
-		var elem = $(id);
-		elem.value = value;
-		elem.onchange();
-	};
-
-	if (timeTable) {
-		updateTime();
+var updateTime = function () {
+	console.log('onUpdateTime');
+	updateTimeOnLine(0);
+};
+var updateTimeOnLine = function (line) {
+	console.log('onUpdateTimeOnLine');
+	for (var column = 2; column <= 6; column++) {
+		setValue('TIME', column, line, time);
 	}
-	if (restTable) {
-		updateRestLunchLocation();
+};
+
+var restTable = $('#UC_EX_TDLY_FR\\$scroll\\$0');
+
+var updateRestLunchLocation = function () {  //methode pour changer la location (cgi, site client...etc)
+	for (var column = 1; column <= 7; column++) {
+		var location = locations[column - 1];
+		updateRestOnColumn(column, location == 'NA' ? 'NA' : 'Y');
+		updateLunchOnColumn(column, location == 'NA' ? 0 : 1);
+		updateLocationOnColumn(column, location);
 	}
 
-    document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click',updateTime());
-});
-    
-})();
+	/* Overtime requested by manager */
+	$('#UC_EX_TIME_FR_H_UC_OT_RQST_MNGR\\$0').value = 0;
+	/* including Sunday hours */
+	$('#UC_EX_TIME_FR_H_UC_OT_RM_SUNDAY\\$0').value = 0;
+	/* including public holidays but Sunday */
+	$('#UC_EX_TIME_FR_H_UC_OT_RM_PUBLIC_HO\\$0').value = 0;
+};
+
+var updateLunchOnColumn = function (column, value) {
+	setValue('UC_TIME_LIN_WRK_UC_DAILYREST1', column, 0, value);
+};
+
+var updateRestOnColumn = function (column, value) {
+	for (var line = 0; line < 3; line++) {
+		setValue('UC_DAILYREST', column, line, value);
+	}
+};
+
+var updateLocationOnColumn = function (column, value) {
+	setValue('UC_LOCATION_A', column, 0, value);
+	setValue('UC_LOCATION_A', column, 1, value);
+};
+
+var setValue = function (name, column, line, value) {
+	var id = '#' + name + column + '$' + line;
+	console.log("test" + id);
+	var elem = $(id);
+	elem.value = value;
+	console.log(elem.value);
+	//elem.onChange();
+};
+
+if (timeTable) {
+	updateTime();
+}
+if (restTable) {
+	updateRestLunchLocation();
+}
+
+//setValue('TIME','2','0',time);
+

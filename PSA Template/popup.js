7770ed9b8c3e2e2dbabb5b7124ae70d517c8e4a6
Julien Peluso <julien.maule@gmail.com>
2019-02-11 11:49:12 +0100

    options now acces with right clic>options

--- PSA Template/popup.js
+++ PSA Template/popup.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({ active: true, url: location });
-            };
-        })();
-    }
-});
-
 $(document).ready(function () {
     function displayBtn() {
         for (var key in localStorage) {
